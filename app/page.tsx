10e64c7b66a06d905a8e0830a41933689e0983e6
Esoraine <evga.abrosimov@gmail.com>
2023-11-23 16:21:44 +0300

    ref

--- app/page.tsx
+++ app/page.tsx
@@ -28,9 +28,9 @@ export default function Page() {
 							</p>
 							<div className='flex items-center self-start mt-2 lg:self-center'>
 								<Link
-									href='/login'
+									href='/auth/login'
 									className='flex items-center bg-baseorange text-light p-2.5 px-6
-                         rounded-lg text-lg font-semibold hover:bg-light hover:baseorange
+                         rounded-lg text-lg font-semibold hover:bg-basecyan hover:baseorange
                          border-2 border-solid border-transparent hover:baseorange md:p-2 md:px-4 md:text-base'
 								>
 									Начать
@@ -55,36 +55,4 @@ export default function Page() {
 			</section>
 		</main>
 	)
-}
-
-{
-	/* <div className='relative isolate px-6 pt-14 lg:px-8'>
-				<div className='mx-auto max-w-2xl py-32 sm:py-48 lg:py-56'>
-					<div className='text-center'>
-						<h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
-							Умный помощник для эффективной работы!
-						</h1>
-						<p className='mt-6 text-lg leading-8 text-gray-600'>
-							Мы инновационный сервис, предназначенный для совместной работы с
-							генеративными чат-ботами на основе модели GPT . Наш помощник
-							поможет вам максимизировать эффективность и результативность
-							использования таких ботов в различных процессах бизнеса.
-						</p>
-						<div className='mt-10 flex items-center justify-center gap-x-6'>
-							<Link
-								href='/auth/login'
-								className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-							>
-								Войдите
-							</Link>
-							<Link
-								href='/auth/registration'
-								className='text-sm font-semibold leading-6 text-gray-900'
-							>
-								или создайте аккаунт <span aria-hidden='true'>&rarr;</span>
-							</Link>
-						</div>
-					</div>
-				</div>
-			</div> */
 }
