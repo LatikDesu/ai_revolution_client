fa46d7a0944806a9059a66df7ef8716c0c858e92
Esoraine <evga.abrosimov@gmail.com>
2023-11-23 16:33:35 +0300

    landing

--- components/screens/landing/layout.tsx
+++ components/screens/landing/layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 const Layout: FC<LayoutProps> = ({ children, className = '' }) => {
 	return (
 		<div
-			className={`w-full h-full inline-block z-0 p-32 xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className}`}
+			className={`w-full h-screen inline-block z-0 pt-32 px-32 xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className}`}
 		>
 			{children}
 		</div>
