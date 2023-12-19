11981b331c211cf4a522a2d19d4932ecdaf66190
Nojotel <nojotel@yandex.ru>
2023-12-19 22:42:29 +0300

    chat and temlate

--- components/forms/Form.tsx
+++ components/forms/Form.tsx
@@ -1,59 +1,101 @@
-import { Spinner } from '@/components/common'
-import { Input } from '@/components/forms'
-import { ChangeEvent, FormEvent } from 'react'
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { Spinner } from "@/components/common";
+import { Input } from "@/components/forms";
+import { ChangeEvent, FormEvent } from "react";
 
 interface Config {
-	labelText: string
-	labelId: string
-	type: string
-	value: string
-	link?: {
-		linkText: string
-		linkUrl: string
-	}
-	required?: boolean
+  labelText: string;
+  labelId: string;
+  type: string;
+  value: string;
+  link?: {
+    linkText: string;
+    linkUrl: string;
+  };
+  required?: boolean;
 }
 
 interface Props {
-	config: Config[]
-	isLoading: boolean
-	btnText: string
-	onChange: (event: ChangeEvent<HTMLInputElement>) => void
-	onSubmit: (event: FormEvent<HTMLFormElement>) => void
+  config: Config[];
+  isLoading: boolean;
+  btnText: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onSubmit: (event: FormEvent<HTMLFormElement>) => void;
+  isButtonActive?: boolean;
 }
 
-export default function Form({
-	config,
-	isLoading,
-	btnText,
-	onChange,
-	onSubmit,
-}: Props) {
-	return (
-		<form className='space-y-6' onSubmit={onSubmit}>
-			{config.map((input) => (
-				<Input
-					key={input.labelId}
-					labelId={input.labelId}
-					type={input.type}
-					onChange={onChange}
-					value={input.value}
-					link={input.link}
-					required={input.required}
-				>
-					{input.labelText}
-				</Input>
-			))}
+export default function Form({ config, isLoading, btnText, onChange, onSubmit }: Props) {
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [isButtonActive, setIsButtonActive] = useState(false);
 
-			<div>
-				<button
-					type='submit'
-					className='flex w-full h-11 justify-center items-center rounded-3xl bg-button px-5 py-3.5 text-sm font-semibold leading-6 text-textlight hover:bg-buttonhover 	focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-buttonhover'
-					disabled={isLoading}
-				>
-					{isLoading ? <Spinner sm /> : `${btnText}`}
-				</button>
-			</div>
-		</form>
-	)
+  useEffect(() => {
+    const initialIsButtonActive = config.every((input) => input.value.trim() !== "" && (input.type.toLowerCase() !== "email" || isEmailValid) && (input.type.toLowerCase() !== "password" || isPasswordValid));
+    setIsButtonActive(initialIsButtonActive);
+  }, [config, isEmailValid, isPasswordValid]);
+
+  useEffect(() => {
+    config.forEach((input) => {
+      if (input.type.toLowerCase() === "email") {
+        const emailValue = input.value.trim();
+        setIsEmailValid(isValidEmail(emailValue));
+      } else if (input.type.toLowerCase() === "password") {
+        const passwordValue = input.value;
+        setIsPasswordValid(passwordValue.length > 6);
+      }
+    });
+  }, [config]);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange(event);
+
+    if (event.target.name === "email") {
+      const emailValue = event.target.value.trim();
+      setIsEmailValid(isValidEmail(emailValue));
+    } else if (event.target.name === "password") {
+      const passwordValue = event.target.value;
+      setIsPasswordValid(passwordValue.length > 6);
+    }
+    const updatedIsButtonActive = config.every((input) => input.value.trim() !== "" && (input.type.toLowerCase() !== "email" || isEmailValid) && (input.type.toLowerCase() !== "password" || isPasswordValid));
+
+    setIsButtonActive(updatedIsButtonActive);
+  };
+
+  const isValidEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  return (
+    <form className="space-y-6" onSubmit={onSubmit}>
+      {config.map((input) => (
+        <Input
+          key={input.labelId}
+          labelId={input.labelId}
+          type={input.type}
+          onChange={handleInputChange}
+          value={input.value}
+          link={input.link}
+          required={input.required}
+          onValidationChange={(isValid) => {
+            if (input.type.toLowerCase() === "email") {
+              setIsEmailValid(isValid);
+            } else if (input.type.toLowerCase() === "password") {
+              setIsPasswordValid(isValid);
+            }
+          }}
+        >
+          {input.labelText}
+        </Input>
+      ))}
+
+      <div>
+        <button type="submit" className={`flex w-full h-11 justify-center items-center rounded-3xl px-5 py-3.5 text-sm font-semibold leading-6 ${isButtonActive ? "bg-button" : "bg-inactive-button border-inactive-button"} text-textlight ${isButtonActive ? "hover:bg-buttonhover" : ""} focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-buttonhover`} disabled={isLoading || !isButtonActive}>
+          {isLoading ? <Spinner sm /> : `${btnText}`}
+        </button>
+      </div>
+    </form>
+  );
 }
--- components/forms/Input.tsx
+++ components/forms/Input.tsx
@@ -1,57 +1,154 @@
-import Link from 'next/link'
-import { ChangeEvent } from 'react'
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { ChangeEvent, useEffect, useState } from "react";
+
+import eyeOpenIcon from "../../public/EyeOpen.svg";
+import eyeClosedIcon from "../../public/EyeClose.svg";
 
 interface Props {
-	labelId: string
-	type: string
-	onChange: (event: ChangeEvent<HTMLInputElement>) => void
-	value: string
-	children: React.ReactNode
-	link?: {
-		linkText: string
-		linkUrl: string
-	}
-	required?: boolean
+  labelId: string;
+  type: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+  children: React.ReactNode;
+  link?: {
+    linkText: string;
+    linkUrl: string;
+  };
+  required?: boolean;
+  onValidationChange: (isValid: boolean) => void;
 }
 
-export default function Input({
-	labelId,
-	type,
-	onChange,
-	value,
-	children,
-	link,
-	required = false,
-}: Props) {
-	return (
-		<>
-			<div className='relative my-8 text-textlight border-b border-textlight border-opacity-50 '>
-				<label
-					htmlFor={labelId}
-					className='absolute top-50% left-1.5 translate-y-1/2 text-sm leading-6 font-normal brightness-50 pointer-events-none'
-				>
-					{children}
-				</label>
-				<input
-					id={labelId}
-					className='bg-transparent w-full text-sm leading-6 font-normal border-none outline-none focus:ring-0 '
-					name={labelId}
-					type={type}
-					onChange={onChange}
-					value={value}
-					required={required}
-				/>
-			</div>
-			{link && (
-				<div className='text-sm'>
-					<Link
-						className='font-normal text-textlight hover:text-texthover'
-						href={link.linkUrl}
-					>
-						{link.linkText}
-					</Link>
-				</div>
-			)}
-		</>
-	)
+export default function Input({ labelId, type, onChange, value, children, link, required = false, onValidationChange }: Props) {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+  const [isValidEmail, setIsValidEmail] = useState(true);
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isEyeHovered, setIsEyeHovered] = useState(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+
+      if (!target.closest(`#${labelId}`)) {
+        validateEmail();
+        setIsHovered(false);
+        setIsClicked(false);
+      }
+    };
+
+    document.body.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.body.removeEventListener("click", handleClickOutside);
+    };
+  }, [labelId, value]);
+
+  const isInputFilled = value.trim() !== "";
+
+  const validateEmail = () => {
+    if (type.toLowerCase() === "password") {
+      setIsValidEmail(true);
+      onValidationChange(true);
+      return;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValid = emailRegex.test(value);
+    setIsValidEmail(isValid);
+
+    if (!isValid) {
+      setIsClicked(false);
+      onValidationChange(false);
+    } else {
+      onValidationChange(true);
+    }
+  };
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
+  return (
+    <>
+      <div
+        className={`relative my-8 text-textlight border-b border-textlight border-opacity-50 ${isHovered || isClicked ? "hovered" : ""}`}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        onClick={() => {
+          setIsClicked(!isClicked);
+        }}
+        style={{
+          borderColor: isValidEmail || value.trim() === "" || (isClicked && document.activeElement?.id === labelId) ? "#4A7AFF" : "#F72E25",
+        }}
+      >
+        <label
+          htmlFor={labelId}
+          className={`absolute left-1.5 text-sm leading-6 font-normal pointer-events-none transition-transform ${isClicked ? "transform-translate" : ""}`}
+          style={{
+            color: (isHovered || isClicked || (typeof document !== "undefined" && document.activeElement?.id === labelId && !isInputFilled)) && !isEyeHovered ? "rgba(246, 246, 246, 0.7)" : "rgba(246, 246, 246, 0.4)",
+            fontSize: isClicked || isInputFilled ? "12px" : "inherit",
+            transform: isClicked ? "translateY(-20px)" : isInputFilled ? "translateY(-20px)" : "translateY(12px)",
+          }}
+        >
+          {children}
+        </label>
+        <input
+          id={labelId}
+          className={`bg-transparent w-full text-sm leading-6 font-normal border-none outline-none focus:ring-0 ${!isValidEmail ? "border-red-500" : ""}`}
+          name={labelId}
+          type={type.toLowerCase() === "password" ? (isPasswordVisible ? "text" : "password") : type}
+          onChange={(e) => {
+            onChange(e);
+          }}
+          value={value}
+          required={required}
+        />
+        {type.toLowerCase() === "password" && (
+          <button
+            type="button"
+            className="absolute right-1.5 top-1/2 transform -translate-y-1/2 cursor-pointer"
+            onClick={togglePasswordVisibility}
+            onMouseEnter={() => setIsEyeHovered(true)}
+            onMouseLeave={() => setIsEyeHovered(false)}
+            style={{
+              backgroundColor: isEyeHovered ? "#3F4657" : "",
+              borderRadius: "50%",
+              width: "25px",
+              height: "25px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginLeft: "20px",
+            }}
+          >
+            {isPasswordVisible ? <Image src={eyeOpenIcon} alt="Open Eye" width={25} height={25} className="eye-icon" /> : <Image src={eyeClosedIcon} alt="Closed Eye" width={25} height={25} className="eye-icon" />}
+          </button>
+        )}
+      </div>
+      {link && (
+        <div className="text-sm">
+          <Link className="font-normal text-textlight hover:text-texthover" href={link.linkUrl}>
+            {link.linkText}
+          </Link>
+        </div>
+      )}
+      <style jsx>{`
+        .hovered label {
+          color: rgba(246, 246, 246, 0.7);
+        }
+
+        .transform-translate {
+          transition: transform 0.3s ease-in-out;
+        }
+
+        .eye-icon {
+          width: 25px;
+          height: 25px;
+        }
+      `}</style>
+    </>
+  );
 }
